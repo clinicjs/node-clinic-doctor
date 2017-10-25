f7699fb69e16324a7638911cc0964cab261c933b
Andreas Madsen <amwebdk@gmail.com>
2017-10-25 10:28:49 +0200

    test interval detection on flat handles

--- test/analysis-guess-interval.test.js
+++ test/analysis-guess-interval.test.js
@@ -4,7 +4,7 @@ const test = require('tap').test
 const guessInterval = require('../analysis/guess-interval.js')
 const generateProcessState = require('./generate-process-state.js')
 
-test('guess interval', function (t) {
+test('guess interval - expected data', function (t) {
   for (const noise of [0, 1, 10]) {
     const data = generateProcessState({
       handles: [3, 3, 3, 3, 3, 13, 13, 13, 13, 13, 13, 13, 3, 3, 3]
@@ -12,6 +12,19 @@ test('guess interval', function (t) {
 
     const interval = guessInterval(data)
     t.strictDeepEqual(interval, [5, 12])
+  }
+
+  t.end()
+})
+
+test('guess interval - flat data', function (t) {
+  for (const noise of [0, 1, 10]) {
+    const data = generateProcessState({
+      handles: [3, 3, 3, 3, 3, 3, 3, 3]
+    }, noise)
+
+    const interval = guessInterval(data)
+    t.strictDeepEqual(interval, [0, 8])
   }
 
   t.end()
