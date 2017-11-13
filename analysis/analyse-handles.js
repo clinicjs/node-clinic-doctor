fd9d2703c2ba8eaae403c778d840a1ba750d748c
Andreas Madsen <amwebdk@gmail.com>
2017-11-13 13:18:52 +0100

    detect handles issue with a differential binomial sign change test

--- analysis/analyse-handles.js
+++ analysis/analyse-handles.js
@@ -4,37 +4,63 @@ const summary = require('summary')
 const distributions = require('distributions')
 
 function analyseHandles (data) {
+  // Healthy handle graphs tends to grow or shrink in small steps.
+  //
+  // handles
+  // |   ^   ^         /^^
+  // |  / \ / \   /\  /   \
+  // | /       \^/  \/     \
+  // ------------------------ time
+  //
+  // Unhealthy handles graphs increase and makes large drop
+  //
+  // handles
+  // |   ^^^   /^^^     /^^^
+  // |  /  |  /    |   /
+  // | /   |^^     |^^^
+  // ------------------------ time
+  //
+  // A heuristic statistical description of such a behaviour, is that
+  // healthy handle graphs are esentially random walks on a symetric
+  // distribution.
+  // To test this, perform a sign change test on the differential.
   const handles = data.map((d) => d.handles)
-  const stats = summary(handles)
-  const studentt = new distributions.Studentt(stats.size() - 1)
 
-  // if there is no variation, the remaning code is statistically going to
-  // be nonsense. But if there is no variation, then it is quite unlikely
-  // there is an I/O issue.
-  if (stats.sd() === 0) return false
+  // Calculate the changes in handles (differential)
+  const changes = diff(handles)
+  // Determin if the change was an increase or a decrease
+  const direction = changes.map(Math.sign)
+  // Count the number of sign changes
+  const signChanges = diff(direction).map((v) => v !== 0)
+  const numSignChanges = summary(signChanges).sum()
 
-  // calculate 5% confidence interval
-  const multiplier = studentt.inv(0.975) // 1 - 5%/2
-  const lowerConfidenceBound = stats.mean() - multiplier * stats.sd()
-  const upperConfidenceBound = stats.mean() + multiplier * stats.sd()
+  // Sign changes on a symetric distribution will follow a binomial distribution
+  // where the properbility of sign change equals 0.5. Thus, perform a binomial
+  // test with the null hypothesis `properbility >= 0.5`.
+  // Note that we don't use a two-sided test because:
+  // 1. It is apparently expensive and complicated to implement.
+  //    See: https://github.com/scipy/scipy/blob/master/scipy/stats/morestats.py
+  //         look for `binom_test`
+  // 2. We currently have no understand of what a consistent sign change
+  //    indicates. Thus we will treat `properbility > 0.5` as being fine.
+  const binomial = new distributions.Binomial(0.5, data.length)
+  const pvalue = binomial.cdf(numSignChanges)
 
-  // count the number of observations outside the confidence interval
-  let below = 0
-  let above = 0
-  for (let i = 0; i < handles.length; i++) {
-    if (handles[i] < lowerConfidenceBound) below += 1
-    else if (handles[i] > upperConfidenceBound) above += 1
-  }
-  const belowRatio = below / stats.size()
-  const aboveRatio = above / stats.size()
-
-  // We should expect 2.5% to be on either side of the confidence interval,
-  // to allow for some error, set the limit to 5%.
-  // This assumes the data is normally distributed. If the data is not normally
-  // distributed the distribution will typically be skewed, thus more
-  // observation will be seen on one of the extream sides. This kind of
-  // distribution is also an indicator of something being odd.
-  return belowRatio > 0.05 || aboveRatio > 0.05
+  // If is is very unlikely that the sign change properbility is greater than
+  // 0.5, then we likely have an issue with the handles.
+  const alpha = 0.001 // risk acceptance
+  return pvalue < alpha
 }
 
 module.exports = analyseHandles
+
+
+function diff (handles) {
+  const changes = []
+  let lastHandles = handles[0]
+  for (let i = 1; i < handles.length; i++) {
+    changes.push(handles[i] - lastHandles)
+    lastHandles = handles[i]
+  }
+  return changes
+}
