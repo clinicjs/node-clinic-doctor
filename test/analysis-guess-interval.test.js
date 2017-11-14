50a3f472938f5a8cec846af026b7ffd58c39bc17
Andreas Madsen <amwebdk@gmail.com>
2017-11-14 16:01:02 +0100

    improve interval guesser
    Include no left-side in the scan
    Check that middle mean is higher than side means

--- test/analysis-guess-interval.test.js
+++ test/analysis-guess-interval.test.js
@@ -5,13 +5,39 @@ const guessInterval = require('../analysis/guess-interval.js')
 const generateProcessState = require('./generate-process-state.js')
 
 test('guess interval - expected data', function (t) {
-  for (const noise of [0, 1, 10]) {
+  for (const noise of [0, 1, 5]) {
     const data = generateProcessState({
       handles: [3, 3, 3, 3, 3, 13, 13, 13, 13, 13, 13, 13, 3, 3, 3]
     }, noise)
 
     const interval = guessInterval(data)
     t.strictDeepEqual(interval, [5, 12])
+  }
+
+  t.end()
+})
+
+test('guess interval - missing left tail', function (t) {
+  for (const noise of [0, 1, 5]) {
+    const data = generateProcessState({
+      handles: [3, 3, 3, 3, 3, 13, 13, 13, 13, 13, 13, 13]
+    }, noise)
+
+    const interval = guessInterval(data)
+    t.strictDeepEqual(interval, [5, 12])
+  }
+
+  t.end()
+})
+
+test('guess interval - missing right tail', function (t) {
+  for (const noise of [0, 1, 5]) {
+    const data = generateProcessState({
+      handles: [13, 13, 13, 13, 13, 3, 3, 3, 3, 3, 3, 3]
+    }, noise)
+
+    const interval = guessInterval(data)
+    t.strictDeepEqual(interval, [0, 5])
   }
 
   t.end()
