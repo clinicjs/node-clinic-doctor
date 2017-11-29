a7ae8898e642e7dc8499a7cc0d9c80c511429023
Andreas Madsen <amwebdk@gmail.com>
2017-11-29 13:19:22 +0100

    update memory analysis test

--- test/analysis-memory.test.js
+++ test/analysis-memory.test.js
@@ -2,43 +2,40 @@
 
 const test = require('tap').test
 const analyseMemory = require('../analysis/analyse-memory.js')
+const generateGCEvent = require('./generate-gc-event.js')
 const generateProcessStat = require('./generate-process-stat.js')
 
-test('analyse memory - sawtooth total heap', function (t) {
-  for (const noise of [0, 1, 10]) {
-    const goodMemory = generateProcessStat({
+test('analyse memory - delay correlation', function (t) {
+  for (const noise of [0 /*, 1, 10 */]) {
+    const goodMemoryStat = generateProcessStat({
       delay: [1, 1, 3, 1, 1, 3, 1, 1, 3],
       memory: {
         heapTotal: [35, 40, 30, 35, 40, 30, 35, 40, 30]
       }
     }, noise)
-    t.strictDeepEquals(analyseMemory(goodMemory, []), {
+    const goodMemoryGc = generateGCEvent([
+       'NONE', 'SCA', 'NONE', 'SCA', 'NONE', 'MSC', 'NONE', 'SCA', 'NONE'
+    ])
+
+    t.strictDeepEquals(analyseMemory(goodMemoryStat, goodMemoryGc), {
       external: false,
       heapTotal: false,
       heapUsed: false,
       rss: false
     })
 
-    const okayMemory = generateProcessStat({
-      delay: [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    const badMemoryStat = generateProcessStat({
+      delay: [1, 1, 20, 1, 1, 20, 1, 1, 20, 1, 1],
       memory: {
-        heapTotal: [50, 70, 30, 50, 70, 30, 50, 70, 30]
+        heapTotal: [50, 70, 30, 50, 70, 30, 50, 70, 30, 50, 30]
       }
     }, noise)
-    t.strictDeepEquals(analyseMemory(okayMemory, []), {
-      external: false,
-      heapTotal: false,
-      heapUsed: false,
-      rss: false
-    })
-
-    const badMemory = generateProcessStat({
-      delay: [1, 1, 10, 1, 1, 10, 1, 1, 10],
-      memory: {
-        heapTotal: [50, 70, 30, 50, 70, 30, 50, 70, 30]
-      }
-    }, noise)
-    t.strictDeepEquals(analyseMemory(badMemory, []), {
+    const badMemoryGc = generateGCEvent([
+       'NONE', 'SCA', 'INC', 'MSC',
+       'SCA', 'INC', 'MSC',
+       'SCA', 'INC', 'MSC', 'NONE'
+    ])
+    t.strictDeepEquals(analyseMemory(badMemoryStat, badMemoryGc), {
       external: false,
       heapTotal: true,
       heapUsed: false,
@@ -53,7 +50,7 @@ test('analyse memory - old space too large', function (t) {
   for (const noise of [0, 1, 10]) {
     const goodMemory = generateProcessStat({
       memory: {
-        heapUsed: [30, 100, 200, 300, 300, 300, 300, 300, 300, 300]
+        heapUsed: [30, 100, 200, 300, 300, 300, 300, 300, 300, 300, 300, 300]
       }
     }, noise)
     t.strictDeepEquals(analyseMemory(goodMemory, []), {
@@ -65,37 +62,7 @@ test('analyse memory - old space too large', function (t) {
 
     const badMemory = generateProcessStat({
       memory: {
-        heapUsed: [30, 100, 200, 300, 400, 500, 600, 700, 800, 900]
-      }
-    }, noise)
-    t.strictDeepEquals(analyseMemory(badMemory, []), {
-      external: false,
-      heapTotal: false,
-      heapUsed: true,
-      rss: false
-    })
-  }
-
-  t.end()
-})
-
-test('analyse memory - huge difference', function (t) {
-  for (const noise of [0, 1, 10]) {
-    const goodMemory = generateProcessStat({
-      memory: {
-        heapUsed: [200, 250, 300, 350, 400, 500, 600, 200, 250, 300]
-      }
-    }, noise)
-    t.strictDeepEquals(analyseMemory(goodMemory, []), {
-      external: false,
-      heapTotal: false,
-      heapUsed: false,
-      rss: false
-    })
-
-    const badMemory = generateProcessStat({
-      memory: {
-        heapUsed: [30, 100, 200, 300, 400, 500, 600, 30, 100, 200]
+        heapUsed: [30, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100]
       }
     }, noise)
     t.strictDeepEquals(analyseMemory(badMemory, []), {
