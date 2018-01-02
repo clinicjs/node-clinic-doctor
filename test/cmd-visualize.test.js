99a6e86693a355dd04dbd394d4804920fa1ee8f1
Andreas Madsen <amwebdk@gmail.com>
2018-01-02 17:33:18 +0100

    test that the full pipeline works

---
+++ test/cmd-visualize.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const fs = require('fs')
+const test = require('tap').test
+const async = require('async')
+const rimraf = require('rimraf')
+const ClinicDoctor = require('../index.js')
+
+test('cmd - test visualization', function (t) {
+  const tool = new ClinicDoctor()
+
+  function cleanup (err, dirname) {
+    t.ifError(err)
+
+    async.parallel([
+      (done) => rimraf(dirname, done),
+      (done) => fs.unlink(dirname + '.html', done)
+    ], function (err) {
+      t.ifError(err)
+      t.end()
+    })
+  }
+
+  tool.collect(
+    [process.execPath, '-e', 'setTimeout(() => {}, 200)'],
+    function (err, dirname) {
+      if (err) return cleanup(err, dirname)
+
+      tool.visualize(dirname, dirname + '.html', function (err) {
+        if (err) return cleanup(err, dirname)
+
+        fs.readFile(dirname + '.html', function (err, content) {
+          if (err) return cleanup(err, dirname)
+
+          t.ok(content.length > 1024)
+          t.end()
+        })
+      })
+    }
+  )
+})
