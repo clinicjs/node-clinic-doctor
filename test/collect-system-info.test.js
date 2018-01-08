9a53a6e0be0555bddad66e051cbf3e9251c62456
Mathias Buus <mathiasbuus@gmail.com>
2018-01-08 12:48:13 +0100

    Add versions to systemInfo (#57)
    * add more debugging info
    
    * add test
    
    * test hrtime as well

---
+++ test/collect-system-info.test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const test = require('tap').test
+const systemInfo = require('../collect/system-info.js')
+
+test('Collect - system info - check data', function (t) {
+  const info = systemInfo()
+
+  t.strictEqual(info.nodeVersions, process.versions)
+  t.strictEqual(info.toolVersion, require('../package').version)
+  t.ok(Array.isArray(info.clock.hrtime))
+  t.ok(Math.abs(Date.now() - info.clock.unixtime) < 1000)
+  t.ok(process.hrtime(info.clock.hrtime)[0] < 2)
+  t.end()
+})
