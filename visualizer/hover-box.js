5a9a9ca1750087ec7737cb762ec719f30ad076c2
Kamil Mech <kamil.mech@nearform.com>
2018-01-10 14:15:43 +0000

    Flip hover (#64)
    * Move y-pos hover logic to `setPosition` and flip when above half way
    
    * Reduce opacity to allow see-through
    
    * Fix typo
    
    * Update formula
    
    * Perform opacity tweak in css
    
    * Perform pointer flip and data-box offset in css
    
    * Replace int literal with corresponding var
    
    * Include `size.pointHeight` in transform

--- visualizer/hover-box.js
+++ visualizer/hover-box.js
@@ -9,22 +9,22 @@ class HoverBox {
     this.showen = false
     this.point = null
 
-    const size = {
+    this.size = {
       titleHeight: 36,
       lineWidth: 1,
       marginTop: 3,
       marginBottom: 3,
-      lengedHeight: 28,
+      legendHeight: 28,
       pointHeight: 10
     }
 
-    const lengedTopOffset = size.titleHeight + size.lineWidth +
-                            size.marginTop
+    const legendTopOffset = this.size.titleHeight + this.size.lineWidth +
+                            this.size.marginTop
 
-    const hoverBoxHeight = lengedTopOffset + size.marginBottom +
-                           this.setup.numLines * size.lengedHeight
+    const hoverBoxHeight = legendTopOffset + this.size.marginBottom +
+                           this.setup.numLines * this.size.legendHeight
 
-    this.height = hoverBoxHeight + size.pointHeight
+    this.height = hoverBoxHeight + this.size.pointHeight
     this.width = 136
 
     // create main svg element
@@ -33,25 +33,34 @@ class HoverBox {
       .attr('width', this.width)
       .attr('height', this.height)
 
+    this.dataBox = this.svg.append('g')
+      .classed('data-box', true)
+
     // create background
-    this.svg.append('rect')
+    this.dataBox.append('rect')
       .classed('background', true)
       .attr('rx', 5)
       .attr('width', this.width)
       .attr('height', hoverBoxHeight)
     this.svg.append('path')
       .classed('pointer', true)
-      .attr('d', `M${this.width / 2 - size.pointHeight} ${hoverBoxHeight} ` +
+      .attr('d', `M${this.width / 2 - this.size.pointHeight} ${hoverBoxHeight} ` +
                  `L${this.width / 2} ${this.height} ` +
-                 `L${this.width / 2 + size.pointHeight} ${hoverBoxHeight} Z`)
-    this.svg.append('rect')
+                 `L${this.width / 2 + this.size.pointHeight} ${hoverBoxHeight} Z`)
+    this.svg.append('path')
+      .classed('pointer', true)
+      .classed('below-curve', true)
+      .attr('d', `M${this.width / 2 - this.size.pointHeight} ${this.size.pointHeight} ` +
+                 `L${this.width / 2} 0 ` +
+                 `L${this.width / 2 + this.size.pointHeight} ${this.size.pointHeight} Z`)
+    this.dataBox.append('rect')
       .classed('line', true)
       .attr('width', this.width)
-      .attr('height', size.lineWidth)
-      .attr('y', size.titleHeight)
+      .attr('height', this.size.lineWidth)
+      .attr('y', this.size.titleHeight)
 
     // create title text
-    this.title = this.svg.append('text')
+    this.title = this.dataBox.append('text')
       .classed('title', true)
       .attr('y', 5)
       .attr('x', this.width / 2)
@@ -60,16 +69,16 @@ class HoverBox {
     // create content text
     this.values = []
     for (let i = 0; i < this.setup.numLines; i++) {
-      this.svg.append('text')
+      this.dataBox.append('text')
         .classed('legend', true)
-        .attr('y', lengedTopOffset + i * size.lengedHeight)
+        .attr('y', legendTopOffset + i * this.size.legendHeight)
         .attr('dy', '1em')
         .attr('x', 12)
         .text(this.setup.shortLegend[i])
 
-      const valueText = this.svg.append('text')
+      const valueText = this.dataBox.append('text')
         .classed('value', true)
-        .attr('y', lengedTopOffset + i * size.lengedHeight)
+        .attr('y', legendTopOffset + i * this.size.legendHeight)
         .attr('dy', '1em')
         .attr('x', 72)
       this.values.push(valueText)
@@ -81,9 +90,19 @@ class HoverBox {
   }
 
   setPosition (x, y) {
+    let belowCurve = false
+    // flip down if above half way
+    if (y - this.height < 0) {
+      belowCurve = true
+      this.dataBox.attr('transform', `translate(0, ${this.size.pointHeight})`)
+    } else {
+      this.dataBox.attr('transform', 'translate(0, 0)')
+    }
     this.svg
-      .style('top', Math.round(y - this.height) + 'px')
+      .style('top', Math.round(belowCurve ? y : y - this.height) + 'px')
       .style('left', Math.round(x - this.width / 2) + 'px')
+      .classed('above-curve', !belowCurve)
+      .classed('below-curve', belowCurve)
   }
 
   setDate (date) {
