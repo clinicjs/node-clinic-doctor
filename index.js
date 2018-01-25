96a79c2c6ea8a15bfe9a7c39ca024eb8197a36ef
Alan Slater <alan.slater@nearform.com>
2018-01-25 15:37:38 +0000

    Don't treat Windows SIGINT exit code as error (#97) (#98)
    * Don't treat Windows SIGINT exit code as error (#97)
    
    * Seperate Windows-only condition so tests can skip it
    
    * Skip the windows-only condition in tests
    
    * Document the issue more completely

--- index.js
+++ index.js
@@ -58,7 +58,12 @@ class ClinicDoctor {
     })
 
     proc.once('exit', function (code, signal) {
-      // the process did not exit normally
+      // Windows exit code STATUS_CONTROL_C_EXIT 0xC000013A returns 3221225786
+      // if not caught. See https://msdn.microsoft.com/en-us/library/cc704588.aspx
+      /* istanbul ignore next: windows hack */
+      if (code === 3221225786 && os.platform() === 'win32') signal = 'SIGINT'
+
+      // Abort if the process did not exit normally.
       if (code !== 0 && signal !== 'SIGINT') {
         if (code !== null) {
           return callback(
