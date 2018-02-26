11e43439aa06bbe0a4a3400e76f32272b0fe8be3
Mathias Buus <mathiasbuus@gmail.com>
2018-02-26 15:08:47 +0100

    support http: true and emit listening (#104)

---
+++ test/cmd-collect-http.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const test = require('tap').test
+const http = require('http')
+const CollectAndRead = require('./collect-and-read.js')
+
+test('cmd - collect - detect server port', function (t) {
+  const cmd = new CollectAndRead({http: true}, '-e', `
+    const http = require('http')
+    http.createServer(onrequest).listen(0)
+
+    function onrequest (req, res) {
+      this.close()
+      res.end('from server')
+    }
+  `)
+
+  cmd.tool.on('listening', function (port) {
+    t.ok(typeof port === 'number')
+    t.ok(port > 0)
+
+    http.get(`http://127.0.0.1:${port}`, function (res) {
+      const buf = []
+      res.on('data', data => buf.push(data))
+      res.on('end', function () {
+        t.deepEquals(Buffer.concat(buf), Buffer.from('from server'))
+        t.end()
+      })
+    })
+  })
+})
