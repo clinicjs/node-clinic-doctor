23e7b0a9ed2ac112439b29a728fd95c53b3e24cb
Mathias Buus <mathiasbuus@gmail.com>
2018-06-25 19:58:12 +0200

    add cb to port event (#131)
    * add cb to port event
    
    * unref fd
    
    * cov
    
    * rerun ci

--- detect-port.js
+++ detect-port.js
@@ -1,9 +1,19 @@
 const onlisten = require('on-net-listen')
 const fs = require('fs')
+const net = require('net')
 
 onlisten(function (addr) {
   this.destroy()
   const port = Buffer.from(addr.port + '')
   fs.writeSync(3, port, 0, port.length)
-  fs.closeSync(3)
+  signal(3, function () {
+    process.exit(0)
+  })
 })
+
+function signal (fd, cb) {
+  const s = new net.Socket({fd, readable: true, writable: false})
+  s.unref()
+  s.on('error', () => {})
+  s.on('close', cb)
+}
