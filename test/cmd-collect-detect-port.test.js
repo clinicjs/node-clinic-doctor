2f0065583766ec4ba009333e840952a4bfcb309c
Mathias Buus <mathiasbuus@gmail.com>
2018-06-25 17:31:51 +0200

    cov

--- test/cmd-collect-detect-port.test.js
+++ test/cmd-collect-detect-port.test.js
@@ -29,3 +29,29 @@ test('cmd - collect - detect server port', function (t) {
     })
   })
 })
+
+test('cmd - collect - detect server port and cb', function (t) {
+  const cmd = new CollectAndRead({detectPort: true}, '-e', `
+    const http = require('http')
+    http.createServer(onrequest).listen(0)
+
+    function onrequest (req, res) {
+      res.end('from server')
+    }
+  `)
+
+  cmd.tool.on('port', function (port, proc, cb) {
+    t.ok(typeof port === 'number')
+    t.ok(port > 0)
+
+    http.get(`http://127.0.0.1:${port}`, function (res) {
+      const buf = []
+      res.on('data', data => buf.push(data))
+      res.on('end', function () {
+        t.deepEquals(Buffer.concat(buf), Buffer.from('from server'))
+        t.end()
+        cb()
+      })
+    })
+  })
+})
