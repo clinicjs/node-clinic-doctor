857039bade3ebc877ffc0d6caf379a85815de958
Mathias Buus <mathiasbuus@gmail.com>
2018-06-25 17:02:46 +0200

    add cb to port event

--- detect-port.js
+++ detect-port.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 onlisten(function (addr) {
   this.destroy()
   const port = Buffer.from(addr.port + '')
+  const buf = Buffer.alloc(1)
   fs.writeSync(3, port, 0, port.length)
-  fs.closeSync(3)
+  fs.read(3, buf, 0, 1, null, function () {
+    process.exit(0)
+  })
 })
