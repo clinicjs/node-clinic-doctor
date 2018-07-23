05041c73b0db72e4cccee6354a732e096b1a58c0
Renée Kooi <renee@kooi.me>
2018-07-23 18:42:13 +0200

    Implement d3.event live binding

--- visualizer/d3.js
+++ visualizer/d3.js
@@ -1,12 +1,18 @@
 'use strict'
 
+const selection = require('d3-selection')
+
 // Reduce file size by only including the d3 modules that are used
 Object.assign(
   module.exports,
+  selection,
   require('d3-array'),
   require('d3-axis'),
   require('d3-scale'),
-  require('d3-selection'),
   require('d3-shape'),
   require('d3-time-format')
 )
+
+Object.assign(module.exports, 'event', {
+  get () { return selection.event }
+})
