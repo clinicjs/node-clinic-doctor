fe0ea1ccf7eb9b44adeb9afab1d1f09ce9c57a17
Renée Kooi <renee@kooi.me>
2018-08-10 10:41:28 +0200

    Fix live binding and export d3 at the end.

--- visualizer/d3.js
+++ visualizer/d3.js
@@ -3,8 +3,8 @@
 const selection = require('d3-selection')
 
 // Reduce file size by only including the d3 modules that are used
-Object.assign(
-  module.exports,
+const d3 = Object.assign(
+  {},
   selection,
   require('d3-array'),
   require('d3-axis'),
@@ -13,6 +13,9 @@ Object.assign(
   require('d3-time-format')
 )
 
-Object.assign(module.exports, 'event', {
+// This property changes after importing so we fake a live binding.
+Object.defineProperty(d3, 'event', {
   get () { return selection.event }
 })
+
+module.exports = d3
