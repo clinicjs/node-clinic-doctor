a3d6f5058007e917c528ae84abf6a96610299680
Renée Kooi <renee@kooi.me>
2018-08-10 10:42:19 +0200

    Add comments noting which methods are used.

--- visualizer/d3.js
+++ visualizer/d3.js
@@ -5,11 +5,23 @@ const selection = require('d3-selection')
 // Reduce file size by only including the d3 modules that are used
 const d3 = Object.assign(
   {},
+  // d3.mouse
+  // d3.select
+  // d3.selectAll
   selection,
+  // d3.min
+  // d3.max
+  // d3.extent
   require('d3-array'),
+  // d3.axisBottom
+  // d3.axisLeft
   require('d3-axis'),
+  // d3.scaleLinear
+  // d3.scaleTime
   require('d3-scale'),
+  // d3.line
   require('d3-shape'),
+  // d3.timeFormat
   require('d3-time-format')
 )
 
