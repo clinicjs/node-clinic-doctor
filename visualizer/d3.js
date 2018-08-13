a8b9236c7040e4537efcfbc33f05e504fe29601a
Renée Kooi <renee@kooi.me>
2018-08-13 10:54:24 +0200

    Implement d3.event live binding (#137)
    * Implement d3.event live binding and export d3 at the end.
    
    * Add comments noting which methods are used.

--- visualizer/d3.js
+++ visualizer/d3.js
@@ -1,12 +1,33 @@
 'use strict'
 
+const selection = require('d3-selection')
+
 // Reduce file size by only including the d3 modules that are used
-Object.assign(
-  module.exports,
+const d3 = Object.assign(
+  {},
+  // d3.mouse
+  // d3.select
+  // d3.selectAll
+  selection,
+  // d3.min
+  // d3.max
+  // d3.extent
   require('d3-array'),
+  // d3.axisBottom
+  // d3.axisLeft
   require('d3-axis'),
+  // d3.scaleLinear
+  // d3.scaleTime
   require('d3-scale'),
-  require('d3-selection'),
+  // d3.line
   require('d3-shape'),
+  // d3.timeFormat
   require('d3-time-format')
 )
+
+// This property changes after importing so we fake a live binding.
+Object.defineProperty(d3, 'event', {
+  get () { return selection.event }
+})
+
+module.exports = d3
