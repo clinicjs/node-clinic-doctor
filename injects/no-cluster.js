fbeb3e89c2e76ba37f465b31430d8ed4243e8aed
Renée Kooi <renee@kooi.me>
2018-08-31 15:23:31 +0200

    bubbleprof → doctor

--- injects/no-cluster.js
+++ injects/no-cluster.js
@@ -1,5 +1,5 @@
 const cluster = require('cluster')
 
 cluster.on('fork', () => {
-  throw new Error('bubbleprof does not support clustering.')
+  throw new Error('clinic doctor does not support clustering.')
 })
