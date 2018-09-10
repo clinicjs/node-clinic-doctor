34f6ed47d7f42753ab4e1c454672426c42bd5892
Luca Maraschi <luca.maraschi@gmail.com>
2018-09-10 11:46:14 -0700

    Augmented tests for path resolution

--- collect/get-logging-paths.js
+++ collect/get-logging-paths.js
@@ -4,14 +4,14 @@ const path = require('path')
 
 function getLoggingPaths (options) {
   let dirpath, basename
-  if (options.hasOwnProperty('identifier')) {
+  if (options.hasOwnProperty('identifier') && options.identifier) {
     if (options.hasOwnProperty('path') && options.path) {
       dirpath = options.path
     } else {
-      dirpath = './'
+      dirpath = ''
     }
     basename = options.identifier.toString()
-  } else if (options.hasOwnProperty('path')) {
+  } else if (options.hasOwnProperty('path') && options.path) {
     dirpath = path.dirname(options.path)
     basename = path.basename(options.path, '.clinic-doctor')
   } else {
