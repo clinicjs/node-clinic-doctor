8666af6af293d098611a0b8dd371d3aeff69558b
Ruben Bridgewater <ruben@bridgewater.de>
2018-09-18 13:43:03 +0200

    Prevent memory exhaustion (#168)
    * Prevent memory exhaustion
    
    This cuts the data streams when the heap runs low on memory. In that
    case a warning will be emitted.
    
    It also fixes some typos in comments.

--- test/cmd-visualize.test.js
+++ test/cmd-visualize.test.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const fs = require('fs')
-const test = require('tap').test
+const v8 = require('v8')
+const { test } = require('tap')
 const async = require('async')
 const rimraf = require('rimraf')
 const ClinicDoctor = require('../index.js')
@@ -12,6 +13,8 @@ test('cmd - test visualization - data exists', function (t) {
   function cleanup (err, dirname) {
     t.ifError(err)
 
+    t.match(dirname, /^foo(\/|\\)[0-9]+\.clinic-doctor$/)
+
     async.parallel([
       (done) => rimraf(dirname, done),
       (done) => fs.unlink(dirname + '.html', done)
@@ -20,6 +23,63 @@ test('cmd - test visualization - data exists', function (t) {
       t.end()
     })
   }
+
+  tool.collect(
+    [process.execPath, '-e', 'setTimeout(() => {}, 200)'],
+    function (err, dirname) {
+      if (err) return cleanup(err, dirname)
+
+      tool.visualize(dirname, dirname + '.html', function (err) {
+        if (err) return cleanup(err, dirname)
+
+        fs.readFile(dirname + '.html', function (err, content) {
+          if (err) return cleanup(err, dirname)
+
+          t.ok(content.length > 1024)
+          cleanup(null, dirname)
+        })
+      })
+    }
+  )
+})
+
+test('cmd - test visualization - memory exhausted', function (t) {
+  const tmp = process.memoryUsage
+  const HEAP_MAX = v8.getHeapStatistics().heap_size_limit
+
+  // Mock the used function to pretend the memory is exhausted.
+  process.memoryUsage = () => {
+    return {
+      rss: 0,
+      heapTotal: HEAP_MAX,
+      heapUsed: 0,
+      external: 0
+    }
+  }
+
+  const tool = new ClinicDoctor()
+
+  function cleanup (err, dirname) {
+    process.memoryUsage = tmp
+    t.ifError(err)
+
+    t.match(dirname, /^[0-9]+\.clinic-doctor$/)
+
+    async.parallel([
+      (done) => rimraf(dirname, done),
+      (done) => fs.unlink(dirname + '.html', done)
+    ], function (err) {
+      t.ifError(err)
+      t.end()
+    })
+  }
+
+  tool.on('warning', function (warning) {
+    t.equal(warning, 'Truncating input data due to memory constrains')
+  })
+  tool.on('truncate', function (undef) {
+    t.equal(undef, undefined)
+  })
 
   tool.collect(
     [process.execPath, '-e', 'setTimeout(() => {}, 200)'],
