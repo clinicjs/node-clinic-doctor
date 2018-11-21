46b0cf30d1829fcca05b506da8ddb1b097e90829
Osmond van Hemert <osmond@vanhemert.nu>
2018-11-21 15:00:28 +0100

    added test for handling exit with code

--- test/cmd-collect-exit.test.js
+++ test/cmd-collect-exit.test.js
@@ -6,6 +6,7 @@ const path = require('path')
 const async = require('async')
 const { spawn } = require('child_process')
 const endpoint = require('endpoint')
+const CollectAndRead = require('./collect-and-read.js')
 
 test('cmd - collect - external SIGINT is relayed', function (t) {
   if (os.platform() === 'win32') {
@@ -34,6 +35,14 @@ test('cmd - collect - external SIGINT is relayed', function (t) {
     t.ok(output.stderr.toString().split('\n').length, 1)
     t.strictEqual(output.stdout.toString(),
       'listening for SIGINT\nSIGINT received\n')
+    t.end()
+  })
+})
+
+test('cmd - collect - non-success exit code should not throw', function (t) {
+  const cmd = new CollectAndRead({}, '--expose-gc', '-e', 'process.exit(1)')
+  cmd.on('error', t.ifError.bind(t))
+  cmd.on('ready', function () {
     t.end()
   })
 })
