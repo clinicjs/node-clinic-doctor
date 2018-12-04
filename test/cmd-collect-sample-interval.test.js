a8b76e08f91f98b9276b27e2f669783b59cabea3
Matteo Collina <hello@matteocollina.com>
2018-12-04 16:40:13 +0100

    print drift

--- test/cmd-collect-sample-interval.test.js
+++ test/cmd-collect-sample-interval.test.js
@@ -38,7 +38,9 @@ test('cmd - collect - custom sample interval', function (t) {
       const sampleTimes = output.processStat.map((stat) => stat.timestamp)
       const timeSeparation = summary(diff(sampleTimes)).mean()
       t.ok(sampleTimes.length > 0, 'data is outputted')
-      t.ok(Math.abs(timeSeparation - 30) < 25)
+      const abs = Math.abs(timeSeparation - 30)
+      t.comment(`drift is ${drift}`)
+      t.ok(drift < 25)
 
       t.end()
     })
