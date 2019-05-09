9877c8d564353b47699b35c23de124a2d2aafed4
Renée Kooi <renee@kooi.me>
2019-05-09 15:09:35 +0200

    fix cluster test

--- test/cmd-no-cluster.script.js
+++ test/cmd-no-cluster.script.js
@@ -1,10 +1,11 @@
 const rimraf = require('rimraf')
+const path = require('path')
 const ClinicDoctor = require('../index.js')
 
 const doctor = new ClinicDoctor()
 doctor.collect([
   process.execPath,
-  '-e', 'var c = require("cluster"); c.isMaster && c.fork()'
+  path.join(__dirname, 'cmd-no-cluster.cluster.js')
 ], (err, result) => {
   rimraf.sync(result)
   if (err) throw err
