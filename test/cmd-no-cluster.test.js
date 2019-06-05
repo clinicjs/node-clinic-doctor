ea1dcaa30422a607414dd512fea355ace9b6d2d9
Andreas Madsen <amwebdk@gmail.com>
2019-06-05 12:46:37 +0200

    fix unstable no-cluster test

--- test/cmd-no-cluster.test.js
+++ test/cmd-no-cluster.test.js
@@ -6,15 +6,16 @@ const endpoint = require('endpoint')
 const rimraf = require('rimraf')
 const ClinicDoctor = require('../index.js')
 
-test('collect command stops when cluster is used', function (t) {
-  t.plan(3)
-
+test('cmd - collect - cluster required but not used is ok', function (t) {
   const doctor = new ClinicDoctor({})
   doctor.collect([process.execPath, '-e', 'require("cluster")'], (err, result) => {
     t.ifError(err, 'should not crash when cluster is required but not used')
     rimraf.sync(result)
+    t.end()
   })
+})
 
+test('cmd - collect - cluster used is not ok', function (t) {
   const proc = spawn(process.execPath, [
     require.resolve('./cmd-no-cluster.script.js')
   ], { stdio: 'pipe' })
@@ -22,5 +23,6 @@ test('collect command stops when cluster is used', function (t) {
   proc.stderr.pipe(endpoint((err, buf) => {
     t.ifError(err)
     t.ok(buf.toString('utf8').includes('does not support clustering'), 'should crash once cluster is used')
+    t.end()
   }))
 })
