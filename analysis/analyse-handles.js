ed919bc8d084de65d8696e5c46fcc8c89f61d809
Andreas Madsen <amwebdk@gmail.com>
2019-06-05 13:25:14 +0200

    implement mira skewness test

--- analysis/analyse-handles.js
+++ analysis/analyse-handles.js
@@ -3,40 +3,7 @@
 const summary = require('summary')
 const distributions = require('distributions')
 
-function analyseHandles (processStatSubset, traceEventSubset) {
-  // Healthy handle graphs tends to grow or shrink in small steps.
-  //
-  // handles
-  // |   ^   ^         /^^
-  // |  / \ / \   /\  /   \
-  // | /       \^/  \/     \
-  // ------------------------ time
-  //
-  // Unhealthy handles graphs increase and makes large drop
-  //
-  // handles
-  // |   ^^^   /^^^     /^^^
-  // |  /  |  /    |   /
-  // | /   |^^     |^^^
-  // ------------------------ time
-  //
-  // A heuristic statistical description of such a behaviour, is that
-  // healthy handle graphs are essentially random walks on a symmetric
-  // distribution.
-  // To test this, perform a sign change test on the differential.
-  const handles = processStatSubset.map((d) => d.handles)
-
-  // Check for stochasticity, otherwise the following is mathematical nonsense.
-  if (summary(handles).sd() === 0) {
-    return false
-  }
-
-  // 1. Assuming handles is a random-walk process, then calculate the first
-  // order auto-diffrential to get the white-noise.
-  // 2. Reduce the dataset to those values where the value did change, this
-  // is to avoid over-sampling and ignore constant-periods.
-  const noise = nonzero(diff(handles))
-
+function whiteNoiseSignTest (noise) {
   // Count the number of sign changes
   const numSignChanges = nonzero(diff(sign(noise))).length
 
@@ -52,10 +19,89 @@ function analyseHandles (processStatSubset, traceEventSubset) {
   const binomial = new distributions.Binomial(0.5, noise.length)
   const pvalue = binomial.cdf(numSignChanges)
 
-  // If is is very unlikely that the sign change probability is greater than
-  // 0.5, then we likely have an issue with the handles.
-  const alpha = 1e-7 // risk acceptance
-  return pvalue < alpha
+  return pvalue
+}
+
+// Implementation of the mira skrewness tests
+// Paper:
+//   Title: Distribution-free test for symmetry based on Bonferroni's Measure
+//   Link: https://www.researchgate.net/publication/2783935
+function miraSkewnessTest (handleStat, handles) {
+  const c = 0.5 // Distribution dependent constant, 0.5 is the best value for unknown distribution
+  const n = handleStat.size()
+  const median = handleStat.median()
+
+  // Compute the bonferroni measure
+  const bonferroni = 2 * (handleStat.mean() - median)
+
+  // Compute the variance for the sqrt(n) * bonferroni measure
+  const medianAbsoluteDeviation = summary(handles.map((x) => Math.abs(x - median))).mean()
+  const densityInverse = (Math.pow(n, 1 / 5) / (2 * c)) * (
+    handleStat.quartile(1 / 2 + 0.5 * Math.pow(n, -1 / 5)) -
+    handleStat.quartile(1 / 2 - 0.5 * Math.pow(n, -1 / 5) + 1 / n)
+  )
+  const bonferroniVariance = 4 * handleStat.variance() +
+                             densityInverse * densityInverse -
+                             4 * densityInverse * medianAbsoluteDeviation
+
+  // Compute Z statistics
+  const Z = Math.sqrt(n) * bonferroni / Math.sqrt(bonferroniVariance)
+
+  // Compute two-sided p-value
+  const normal = new distributions.Normal()
+  const pvalue = 2 * (1 - normal.cdf(Math.abs(Z)))
+
+  return pvalue
+}
+
+function analyseHandles (processStatSubset, traceEventSubset) {
+  // Healthy handle graphs tends to grow or shrink in small steps.
+  //
+  // handles
+  // |   ^   ^         /^^
+  // |  / \ / \   /\  /   \
+  // | /       \^/  \/     \
+  // ------------------------ time
+  //
+  // Unhealthy handles graphs increase and makes large drop (sawtooth pattern)
+  //
+  // handles
+  // |   ^^^   /^^^     /^^^
+  // |  /  |  /    |   /
+  // | /   |^^     |^^^
+  // ------------------------ time
+  //
+  // A heuristic statistical description of such a behaviour, is that
+  // healthy handle graphs are essentially random walks on a symmetric
+  // distribution.
+  // To test this, perform a sign change test on the differential.
+  const handles = processStatSubset.map((d) => d.handles)
+  const handleStat = summary(handles)
+
+  // Check for stochasticity, otherwise the following is mathematical nonsense.
+  if (handleStat.sd() === 0) {
+    return false
+  }
+
+  // 1. Assuming handles is a random-walk process, then calculate the first
+  // order auto-diffrential to get the white-noise.
+  // 2. Reduce the dataset to those values where the value did change, this
+  // is to avoid over-sampling and ignore constant-periods.
+  const noise = nonzero(diff(handles))
+
+  // Perform a sign test on the assumed-noise.
+  // This is to test the sawtooth pattern.
+  const signAlpha = 1e-7 // risk acceptance
+  const signPvalue = whiteNoiseSignTest(noise)
+  const signIssueDetected = signPvalue < signAlpha
+
+  // Perform a two-sided mira-skewness-test.
+  // This is to test an increasing function, that eventually flattens out
+  const miraAlpha = 1e-10 // risk acceptance
+  const miraPvalue = miraSkewnessTest(handleStat, handles)
+  const miraIssueDetected = miraPvalue < miraAlpha
+
+  return signIssueDetected || miraIssueDetected
 }
 
 module.exports = analyseHandles
