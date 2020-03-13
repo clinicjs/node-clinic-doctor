43838bd8424c525405bcdc0b43b51606228604d0
Dylan <dylan_coakley@yahoo.co.uk>
2020-03-13 10:48:41 +0000

    Add new test files for transpile warn

--- test/cmd-collect-source-warning.test.js
+++ test/cmd-collect-source-warning.test.js
@@ -1,16 +1,19 @@
 'use strict'
 
 const test = require('tap').test
-const path = require('path')
-const rimraf = require('rimraf')
-const ClinicDoctor = require('../index.js')
+const { spawn } = require('child_process')
+const endpoint = require('endpoint')
 
 test('cmd - collect - source warning', function (t) {
-  const doctor = new ClinicDoctor({})
-  const filename = path.join('test', 'fixtures', 'plain-with-sourcemap.js')
-  doctor.collect([process.execPath, filename], (err, result) => {
-    t.ifError(err, 'should not crash when attempting source map detection')
-    rimraf.sync(result)
+  const proc = spawn(process.execPath, [
+    require.resolve(__dirname+'/fixtures/sourcemap.run.js')
+  ])
+  
+  proc.stderr.pipe(endpoint((err, buf) => {
+  	console.log('BUF PLAIN', buf);
+  	console.log('BUF', buf.toString('utf8'));
+    t.ifError(err)
+    t.ok(buf.toString('utf8').includes('Transpiled code is not supported'), 'should warn if transpiled code is used')
     t.end()
-  })
+  }))
 })
---
+++ test/fixtures/sourcemap.run.js
@@ -0,0 +1,12 @@
+const rimraf = require('rimraf')
+const path = require('path')
+const ClinicDoctor = require('../../index.js')
+
+const doctor = new ClinicDoctor()
+doctor.collect([
+  process.execPath,
+  path.join(__dirname, 'plain-with-sourcemap.js')
+], (err, result) => {
+  rimraf.sync(result)
+  if (err) throw err
+})
