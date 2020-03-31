836e7d45d07519234c164aef1515e21307782d32
Renée Kooi <renee@kooi.me>
2020-03-31 10:57:10 +0200

    Output data into a separate script tag.

--- index.js
+++ index.js
@@ -139,7 +139,6 @@ class ClinicDoctor extends events.EventEmitter {
   }
 
   visualize (dataDirname, outputFilename, callback) {
-    const fakeDataPath = path.join(__dirname, 'visualizer', 'data.json')
     const stylePath = path.join(__dirname, 'visualizer', 'style.css')
     const scriptPath = path.join(__dirname, 'visualizer', 'main.js')
     const logoPath = path.join(__dirname, 'visualizer', 'app-logo.svg')
@@ -222,11 +221,7 @@ class ClinicDoctor extends events.EventEmitter {
     let scriptFile = buildJs({
       basedir: __dirname,
       debug: this.debug,
-      fakeDataPath,
-      scriptPath,
-      beforeBundle: b => b.require(dataFile, {
-        file: fakeDataPath
-      })
+      scriptPath
     })
 
     if (!this.debug) {
@@ -238,10 +233,6 @@ class ClinicDoctor extends events.EventEmitter {
       stylePath,
       debug: this.debug
     })
-
-    // forward dataFile errors to the scriptFile explicitly
-    // we cannot use destroy until nodejs/node#18172 and nodejs/node#18171 are fixed
-    dataFile.on('error', (err) => scriptFile.emit('error', err))
 
     // Create body contents with recommendations
     const body = streamTemplate`
@@ -262,6 +253,7 @@ class ClinicDoctor extends events.EventEmitter {
       favicon: clinicFaviconBase64,
       title: 'Clinic Doctor',
       styles: styleFile,
+      data: dataFile,
       script: scriptFile,
       headerLogoUrl: 'https://github.com/nearform/node-clinic-doctor',
       headerLogoTitle: 'Clinic Doctor on GitHub',
