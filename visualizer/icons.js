1ad5cb59a94266073bcf9e885a7f52c981dadbe5
ConorDavenport <cnrdavenport@gmail.com>
2020-04-17 15:04:07 +0100

    remove unnecessary quotation

--- visualizer/icons.js
+++ visualizer/icons.js
@@ -46,7 +46,7 @@ class Icons {
       'arrow-right': sanitizeIcon(require('@nearform/clinic-common/icons/chevron-right')),
       'arrow-left': sanitizeIcon(require('@nearform/clinic-common/icons/chevron-left')),
       close: sanitizeIcon(require('@nearform/clinic-common/icons/close')),
-      'copy': sanitizeIcon(require('@nearform/clinic-common/icons/copy')),
+      copy: sanitizeIcon(require('@nearform/clinic-common/icons/copy')),
       'grid-1x4': sanitizeIcon(require('@nearform/clinic-common/icons/list-view')),
       'grid-2x2': sanitizeIcon(require('@nearform/clinic-common/icons/grid-view')),
       theme: sanitizeIcon(require('@nearform/clinic-common/icons/eye-show')),
