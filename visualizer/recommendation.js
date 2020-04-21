e99c3f15710dc9180f0a3c05718c8ad34910898c
ConorDavenport <cnrdavenport@gmail.com>
2020-04-21 16:37:16 +0100

    move formatSnippet() call to draw() function

--- visualizer/recommendation.js
+++ visualizer/recommendation.js
@@ -247,7 +247,7 @@ class Recomendation extends EventEmitter {
           headerElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
         })
     }
-
+    this.formatSnippet()
     // set space height such that the fixed element don't have to hide
     // something in the background.
     this.space.style('height', this.details.node().offsetHeight + 'px')
