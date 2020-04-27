e300871f86e6af4a26f7adbe7c230fb99f64fbb7
ConorDavenport <cnrdavenport@gmail.com>
2020-04-27 17:10:35 +0100

    add test for getIssue function

---
+++ test/get-issue.test.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const fs = require('fs')
+const { test } = require('tap')
+const async = require('async')
+const rimraf = require('rimraf')
+const ClinicDoctor = require('../index.js')
+
+test('get issue', function (t) {
+  const tool = new ClinicDoctor({ dest: './foo' })
+
+  function cleanup (err, dirname) {
+    t.ifError(err)
+
+    t.match(dirname, /^foo(\/|\\)[0-9]+\.clinic-doctor$/)
+
+    async.parallel([
+      (done) => rimraf(dirname, done),
+      (done) => fs.unlink(dirname + '.html', done)
+    ], function (err) {
+      t.ifError(err)
+      t.end()
+    })
+  }
+
+  const script =
+  'setTimeout(() => {}, 1000)'
+
+  tool.collect(
+    [process.execPath, '-e', script],
+    function (err, dirname) {
+      if (err) return cleanup(err, dirname)
+
+      tool.visualize(dirname, dirname + '.html', function (err) {
+        if (err) return cleanup(err, dirname)
+
+        const issue = tool.getIssue()
+        t.equal(issue, 'data')
+        cleanup(null, dirname)
+      })
+    }
+  )
+})
