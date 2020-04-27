3e256060a02336aa5f31e01858c13042ac964b86
ConorDavenport <cnrdavenport@gmail.com>
2020-04-27 15:34:06 +0100

    update test with toolVersion

--- test/format-system-info.test.js
+++ test/format-system-info.test.js
@@ -25,7 +25,8 @@ test('Format - system info - decoding', function (t) {
         unixtime: 33000000
       },
       clockOffset: 32999999.6,
-      nodeVersion: semver.parse(process.versions.node)
+      nodeVersion: semver.parse(process.versions.node),
+      toolVersion: semver.parse(require('../package').version)
     })
 
     t.end()
